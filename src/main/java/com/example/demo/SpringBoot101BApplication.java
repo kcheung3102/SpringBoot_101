576f5331426154be187bf3ac9532749dfaf43ba2
kcheung3102 <kcheung3102@gmail.com>
2019-10-07 11:06:08 -0400

    initial commit

---
+++ src/main/java/com/example/demo/SpringBoot101BApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot101BApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot101BApplication.class, args);
+    }
+
+}
